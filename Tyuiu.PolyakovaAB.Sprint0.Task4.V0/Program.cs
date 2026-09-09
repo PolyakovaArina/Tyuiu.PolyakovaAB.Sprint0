a0f033e70e2deee2c96ffb6c71b583c378c3ebde
PolyakovaAB <Tyuiu.PolyakovaA@outlook.com>
2026-09-09 23:29:02 +0500

    Test4

---
+++ Tyuiu.PolyakovaAB.Sprint0.Task4.V0/Program.cs
@@ -0,0 +1,16 @@
+﻿using Tyuiu.PolyakovaAB.Sprint0.Task4.V0.Lib;
+namespace Tyuiu.PolyakovaAB.Sprint0.Task4.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DataService.Addition(5,5));
+            Console.WriteLine(DataService.Substraction(5, 5));
+            Console.WriteLine(DataService.Multiplication(5, 5));
+            Console.WriteLine(DataService.Division(5, 5));
+            Console.ReadKey();
+
+        }
+    }
+}
