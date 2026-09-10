1754af2481d36fec9223fb4a08d92deaa6c1c8cb
PolyakovaAB <Tyuiu.PolyakovaA@outlook.com>
2026-09-10 19:59:34 +0500

    Task5

---
+++ Tyuiu.PolyakovaAB.Sprint0.Task5.V0.Test/DataServiceTest.cs
@@ -0,0 +1,28 @@
+﻿using Tyuiu.PolyakovaAB.Sprint0.Task5.V0.Lib;
+namespace Tyuiu.PolyakovaAB.Sprint0.Task5.V0.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckAdditionValid()
+        {
+            Assert.AreEqual(10, DataService.Addition(5, 5));
+        }
+        [TestMethod]
+        public void CheckSubstractionalVoid()
+        {
+            Assert.AreEqual(0, DataService.Substraction(5, 5));
+        }
+        [TestMethod]
+        public void CheckMultiplicationVoid()
+        {
+            Assert.AreEqual(25, DataService.Multiplication(5, 5));
+        }
+        [TestMethod]
+        public void CheckDivisionVoid()
+        {
+            Assert.AreEqual(1, DataService.Division(5, 5));
+        }
+    }
+}
---
+++ Tyuiu.PolyakovaAB.Sprint0.Task5.V0/Program.cs
@@ -0,0 +1,15 @@
+﻿using Tyuiu.PolyakovaAB.Sprint0.Task5.V0.Lib;
+namespace Tyuiu.PolyakovaAB.Sprint0.Task5.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("A + B = "+DataService.Addition(5, 5));
+            Console.WriteLine("A - B = " + DataService.Substraction(5, 5));
+            Console.WriteLine("A * B = " + DataService.Multiplication(5, 5));
+            Console.WriteLine("A / B = " + DataService.Division(5, 0));
+            Console.ReadKey();
+        }
+    }
+}
